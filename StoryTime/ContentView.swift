b01cc6ba2d9718ced3913c71561a692fade80077
Matt <matthewsimonmould@gmail.com>
2020-04-24 09:34:46 +0100

    Initial Commit

---
+++ StoryTime/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  StoryTime
+//
+//  Created by Matthew Mould on 24/04/2020.
+//  Copyright © 2020 Matthew Mould. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
