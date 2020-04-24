d9d01e701b99d69f92fc2f6587e3262b43120b77
Matt <matthewsimonmould@gmail.com>
2020-04-24 09:41:31 +0100

    Get rid of camel case

--- StorytimeTests/StorytimeTests.swift
+++ StorytimeTests/StorytimeTests.swift
@@ -7,9 +7,9 @@
 //
 
 import XCTest
-@testable import StoryTime
+@testable import Storytime
 
-class StoryTimeTests: XCTestCase {
+class StorytimeTests: XCTestCase {
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
