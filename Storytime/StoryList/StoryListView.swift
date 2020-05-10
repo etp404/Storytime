0ae30d6e9bda100755be86d569b1e1df036bc0b2
Matt <matthewsimonmould@gmail.com>
2020-05-10 10:00:57 +0100

    Teasing out the NavigationLink

--- Storytime/StoryList/StoryListView.swift
+++ Storytime/StoryList/StoryListView.swift
@@ -21,14 +21,16 @@ struct StoryListView: View {
         GeometryReader { geometry in
             NavigationView {
                 ZStack {
-                    ForEach(self.viewModel.storiesInStack, id: \.storyId) {(story:StoryViewModel) in
-                        Card(story:story) {
-                            storyId in
-                            self.viewModel.dismissStory(id: storyId)
-                        }
-                        .animation(.spring())
-                        .offset(x: CGFloat(-story.index * 5), y: CGFloat(-story.index * 10))
-                        .zIndex(-Double(story.index))
+                    ForEach(self.viewModel.storiesInStack, id: \.storyId){
+                        (story:StoryViewModel) in
+                        VStack {
+                            Card(story:story) {
+                                storyId in
+                                self.viewModel.dismissStory(id: storyId)
+                            }
+                            .animation(.spring())
+                            .offset(x: CGFloat(-story.index * 5), y: CGFloat(-story.index * 10))
+                        }.zIndex(-Double(story.index))
                     }
                 }
             }
@@ -40,6 +42,7 @@ struct Card : View {
     @State private var translation: CGSize = .zero
     private let onDismiss:(UUID)->Void
     private let story:StoryViewModel
+    @State private var isPresented = false
     
     init(story:StoryViewModel, onDismiss: @escaping (UUID)->Void) {
         self.story = story
@@ -47,32 +50,39 @@ struct Card : View {
     }
     
     var body: some View{
-        GeometryReader { geometry in
-            VStack() {
-                Text(self.story.title)
-                    .font(.title)
-                    .bold()
-            }
-            .padding(.horizontal)
-            .frame(width:  geometry.size.width*0.8, height:  geometry.size.height*0.8, alignment: .center)
-            .background(Color.white)
-            .cornerRadius(10)
-            .shadow(radius: 5)
-            .offset(x: self.translation.width, y: self.translation.height)
-            .animation(.interactiveSpring())
-            .rotationEffect(.degrees(Double(self.translation.width / geometry.size.width) * 25), anchor: .bottom)
-            .gesture(
-                DragGesture()
-                    .onChanged { gesture in
-                        self.translation = gesture.translation
-                }.onEnded { gesture in
-                    if self.shouldDismiss(geometry, gesture: gesture) {
-                        self.onDismiss(self.story.storyId)
-                    } else {
-                        self.translation = .zero
-                    }
+        Button(action: {
+            self.isPresented = true
+        }) {
+            GeometryReader { geometry in
+                VStack() {
+                    Text(self.story.title)
+                        .font(.title)
+                        .bold()
                 }
-            )
+                .padding(.horizontal)
+                .frame(width:  geometry.size.width*0.8, height:  geometry.size.height*0.8, alignment: .center)
+                .background(Color.white)
+                .cornerRadius(10)
+                .shadow(radius: 5)
+                .offset(x: self.translation.width, y: self.translation.height)
+                .animation(.interactiveSpring())
+                .rotationEffect(.degrees(Double(self.translation.width / geometry.size.width) * 25), anchor: .bottom)
+                .gesture(
+                    DragGesture()
+                        .onChanged { gesture in
+                            self.translation = gesture.translation
+                    }.onEnded { gesture in
+                        if self.shouldDismiss(geometry, gesture: gesture) {
+                            self.onDismiss(self.story.storyId)
+                        } else {
+                            self.translation = .zero
+                        }
+                    }
+                )
+                    .background(NavigationLink(destination: WholeStoryView(), isActive: self.$isPresented) {
+                        EmptyView()
+                })
+            }
         }
     }
     
@@ -80,7 +90,6 @@ struct Card : View {
         abs(gesture.translation.width / geometry.size.width) > 0.5
     }
 }
-
 
 struct StoryListView_Previews: PreviewProvider {
     static var previews: some View {
