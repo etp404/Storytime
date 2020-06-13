74cb3ec0651135fcd626bcd7e2e15dcb8d4c09b2
Matt <matthewsimonmould@gmail.com>
2020-06-13 18:20:46 +0100

    Navigate to whole story from my story list

--- Storytime/MyStories/MyStoriesView.swift
+++ Storytime/MyStories/MyStoriesView.swift
@@ -11,17 +11,21 @@ import SwiftUI
 struct MyStoriesView: View {
     
     let viewModel: MyStoriesViewModel
-    
+    private let navigation:StorytimeViewNavigation
+
     init() {
         let storyTimeModel = StubbedStorytimeModel()
         viewModel = MyStoriesViewModel(model:storyTimeModel)
+        navigation = StorytimeViewNavigation(storytimeModel: storyTimeModel)
     }
     
     var body: some View {
         NavigationView {
             List {
                 ForEach(viewModel.myStories, id: \.id) {story in
-                    Text(story.title)
+                    NavigationLink(destination: WholeStoryView(wholeStoryViewModel: self.navigation.wholeStoryViewModel(id: story.id))) {
+                        Text(story.title)
+                    }
                 }
             }
             .navigationBarTitle(Text("My Stories"), displayMode: .inline)
--- Storytime/WholeStory/WholeStoryView.swift
+++ Storytime/WholeStory/WholeStoryView.swift
@@ -18,17 +18,19 @@ struct WholeStoryView: View {
     }
 
     var body: some View {
-        List {
-            ForEach(wholeStoryViewModel.content, id: \.self) {section in
-                Text(section)
-                    .padding(.all)
-                    .background(Color.white)
-                    .cornerRadius(10)
-                    .shadow(radius: 5)
-                    .allowsTightening(true)
+        GeometryReader { geometry in
+            List {
+                ForEach(self.wholeStoryViewModel.content, id: \.self) {section in
+                    Text(section)
+                        .frame(width:  geometry.size.width*0.8, height:  nil)
+                        .padding(.all)
+                        .background(Color.white)
+                        .cornerRadius(10)
+                        .shadow(radius: 5)
+                }
             }
+            .navigationBarTitle(Text(self.wholeStoryViewModel.title), displayMode: .inline)
         }
-        .navigationBarTitle(Text(wholeStoryViewModel.title), displayMode: .inline)
     }
 }
 
