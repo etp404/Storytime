bfa89bb3c165c1c4f5d291976367fe286e38f5cb
Matt Mould <matt.mould@bbc.co.uk>
2020-08-14 15:12:13 +0100

    Revert "Get it working for iOS 14"
    This reverts commit 8e181399fba8ba005255b534b203dc7be6a3562a.

--- Storytime/Compose/ComposeView.swift
+++ Storytime/Compose/ComposeView.swift
@@ -9,11 +9,8 @@
 import SwiftUI
 
 struct ComposeView: View {
-
-    @State var story: String = ""
-
     var body: some View {
-        TextField("Compose story...", text:$story)
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
