8e181399fba8ba005255b534b203dc7be6a3562a
Matt Mould <matt.mould@bbc.co.uk>
2020-08-14 14:55:42 +0100

    Get it working for iOS 14

--- Storytime/Compose/ComposeView.swift
+++ Storytime/Compose/ComposeView.swift
@@ -9,8 +9,11 @@
 import SwiftUI
 
 struct ComposeView: View {
+
+    @State var story: String = ""
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        TextField("Compose story...", text:$story)
     }
 }
 
